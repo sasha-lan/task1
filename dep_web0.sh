633d15dc0236f1c2f9b06f651780c6185a74fc69
sasha-lan <46924947+sasha-lan@users.noreply.github.com>
2024-04-14 17:10:08 +0300

    Update dep_web0.sh

--- dep_web0.sh
+++ dep_web0.sh
@@ -7,7 +7,7 @@ else
  apt update
  apt install git -y
  git clone https://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@github.com/sasha-lan/task1.git
- echo "git installed" >> ~/report.txt
+ #echo "git installed" >> ~/report.txt
  chown -R $USER:$USER ~/deploy_web
  chmod +x ~/deploy_web/*.sh
  
